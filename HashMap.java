96ceda2b74e74e91b803ee029db129c928842fef
Daniel Pradera <Daniel Pradera>
2016-03-07 12:32:33 +0100

    Métodos vacios

---
+++ HashMap.java
@@ -0,0 +1,59 @@
+
+/**
+ * Write a description of class HashMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HashMap
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class HashMap
+     */
+    public HashMap()
+    {
+    }
+    
+    public void put(String clave, int valor)
+    {
+    
+    }
+    
+    public void get(String clave)
+    {
+    
+    }
+    
+    public void isEmpty()
+    {
+    
+    }
+    
+    public void size()
+    {
+    
+    }
+    
+    public void remove(String clave)
+    {
+    
+    }
+    
+    public void clear()
+    {
+    
+    }
+   
+    public void containsKey(String clave)
+    {
+    
+    }
+    
+    public void containsValue(int valor)
+    {
+    
+    }
+}
